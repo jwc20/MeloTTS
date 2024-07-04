fd242ecda379499359e71521e4c77c6944a33d00
cjw <choi.jae100@gmail.com>
2024-07-04 21:16:40 +0900

    Add cors middleware, remove semicolons and other symbols

--- tts_server.py
+++ tts_server.py
@@ -1,14 +1,38 @@
 import uvicorn
 from fastapi import FastAPI, Request
-
+# from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware 
+from pprint import pprint
 from melo.api import TTS
-
+import string
 
 speed = 1.0
 device = "cuda:0"
 model = TTS(language="KR", device=device)
+speaker_ids = model.hps.data.spk2id
 
 app = FastAPI()
+
+punc = ''';:@#$%^&*_-~※○●□■△▲◇◆▽▼→←↑↓↔↕↗↘↙↖↙↗↘↖↔↕─│┌┐└┘├┤┬┴┼━┃┏┓┗'''
+
+# allowed_origins = [
+#     "http://localhost.tiangolo.com",
+#     "https://localhost.tiangolo.com",
+#     "http://localhost",
+#     "http://localhost:8080",
+#     "https://localhost:44345/",
+#     "https://localhost:44345",
+# ]
+
+allowed_origins=["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["X-Requested-With", "Content-Type"],
+    allow_origins=allowed_origins,
+)
 
 
 # from apitally.flask import ApitallyMiddleware
@@ -28,9 +52,16 @@ async def root():
 @app.post("/api/synthesize")
 async def apifunction_generate_tts(request: Request):
     _json = await request.json()
+    pprint(_json)
     text = _json["text"]
+    print(text)
 
-    speaker_ids = model.hps.data.spk2id
+    for c in text:
+        if c in punc:
+            text = text.replace(c, "")
+    
+    text = text.replace('\n', ' ')
+    
     response_data = model.tts_to_base64(text, speaker_ids["KR"], speed=speed)
     return response_data
 
