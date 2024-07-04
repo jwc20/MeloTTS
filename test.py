19dd1f0b13575da2913bc40473ee01742ed2c7af
cjw <choi.jae100@gmail.com>
2024-07-04 15:09:45 +0900

    Add test file

---
+++ test.py
@@ -0,0 +1,34 @@
+from melo.api import TTS
+from datetime import datetime
+
+
+# Speed is adjustable
+speed = 1.0
+# device = "cpu"  # or cuda:0
+device = "cuda:0"
+
+
+start_time = datetime.now()
+
+# text = "안녕하세요! 오늘은 날씨가 정말 좋네요."
+
+txt1 = "1번. 또 파이썬은 순수한 프로그램 언어로서의 기능 외에도 다른 언어로 쓰인 모듈들을 연결하는 접착제 언어로써 자주 이용된다. 실제 파이썬은 많은 상용 응용 프로그램에서 스크립트 언어로 채용되고 있다. 도움말 문서도 정리가 잘 되어 있으며, 유니코드 문자열을 지원해서 다양한 언어의 문자 처리에도 능하다."
+txt2 = "2번. 파이썬에서는 들여쓰기를 사용해서 블록을 구분하는 독특한 문법을 채용하고 있다. 이 문법은 파이썬에 익숙한 사용자나 기존 프로그래밍 언어에서 들여쓰기의 중요성을 높이 평가하는 사용자에게는 잘 받아들여지고 있지만, 다른 언어의 사용자에게서는 프로그래머의 코딩 스타일을 제한한다는 비판도 많다."
+txt3 = "3번. 파이썬은 1980년대 말 고안되어 네덜란드 CWI의 귀도 반 로섬이 1989년 12월 구현하기 시작하였다. 이는 역시 SETL에서 영감을 받은 ABC 언어의 후계로서 예외 처리가 가능하고, 아메바 OS와 연동이 가능하였다. 반 로섬은 파이썬의 주 저자로 계속 중심적 역할을 맡아 파이썬의 방향을 결정하여, 파이썬 공동체로부터 자선 종신 이사의 칭호를 부여받았다. 이 같은 예로는 리눅스의 리누스 토발즈 등이 있다."
+
+arr = [txt1, txt2, txt3]
+
+
+model = TTS(language="KR", device=device)
+speaker_ids = model.hps.data.spk2id
+
+
+for i, t in enumerate(arr):
+    print(i)
+    date = datetime.now()
+    date_format = date.strftime("%Y%m%d_%H%M%S")
+    
+    model.tts_to_file(t, speaker_ids["KR"], "kr_demo_" + str(date_format) + "_" + str(i) + ".wav", speed=speed)
+
+
+print(datetime.now() - start_time)
